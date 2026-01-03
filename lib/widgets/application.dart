f2de76ad4a4a56370494b3e7801bcb6eb26b1680
Vilgefortz13 <rinatuzanzaev0@gmail.com>
2026-01-03 12:24:50 +0600

    styled tab bar

--- lib/widgets/application.dart
+++ lib/widgets/application.dart
@@ -15,13 +15,27 @@ class Application extends StatelessWidget {
         bottomNavigationBarTheme: const BottomNavigationBarThemeData(
           backgroundColor: Color.fromARGB(255, 54, 54, 54),
         ),
+        tabBarTheme: TabBarThemeData(
+          labelStyle: TextStyle(
+            color: Colors.white70,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+          unselectedLabelStyle: TextStyle(
+            color: Colors.white24,
+            fontSize: 18,
+            fontWeight: FontWeight.w400,
+          ),
+          dividerColor: Colors.transparent,
+          indicatorColor: Colors.transparent,
+        ),
         textTheme: TextTheme(
           bodyMedium: TextStyle(fontSize: 22, color: Colors.white70),
           titleLarge: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w500,
             color: const Color.fromARGB(255, 231, 231, 231),
-          )
+          ),
         ),
       ),
       home: MainBottomBar(),
