2de00d9d2364523a86ab92ddcdef6696a6cc7897
gloxinia13 <uzanzaev44@gmail.com>
2026-06-10 21:57:54 +0600

    style the screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,17 @@ class Application extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFF1C1A1B),
-        appBarTheme: AppBarThemeData(
-          backgroundColor: const Color(0xFF1c1a1b)
+        textTheme: TextTheme(
+          titleMedium: TextStyle(
+            color: Color.fromARGB(255, 193, 193, 193),
+            fontSize: 20,
+          ),
         ),
+        scaffoldBackgroundColor: Color(0xFF1C1A1B),
+        appBarTheme: AppBarThemeData(backgroundColor: const Color(0xFF1c1a1b)),
         bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: const Color(0xFF1c1a1b)
-        )
+          backgroundColor: const Color(0xFF585657),
+        ),
       ),
       title: 'Financial Accounting',
       home: const MainBottomAppBar(),
@@ -48,17 +52,22 @@ class _MainBottomAppBarState extends State<MainBottomAppBar> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
+      appBar: AppBar(title: Text('Home', style: theme.textTheme.titleMedium)),
       body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Abs'),
-          BottomNavigationBarItem(icon: Icon(Icons.fast_forward), label: 'Horor'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fast_forward),
+            label: 'Horor',
+          ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Color(0xFFd9d9d9),
         onTap: _onItemTapped,
       ),
     );
